09e47b68e101c08f85b83e28dd268ed4ea55d50b
memelamc <mmemela@csir.co.za>
2021-03-16 21:24:45 +0200

    Create User.cs

---
+++ Task_02/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_02.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+        public string Gender { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
