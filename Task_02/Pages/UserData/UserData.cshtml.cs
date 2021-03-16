6599a169b9d671616526a4bedada8b30e64e8e61
memelamc <mmemela@csir.co.za>
2021-03-16 21:48:38 +0200

    Testing User Model using Form and OnPost

--- Task_02/Pages/UserData/UserData.cshtml.cs
+++ Task_02/Pages/UserData/UserData.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Task_02.Models;
 
 namespace Task_02.Pages.UserData
 {
     public class UserDataModel : PageModel
     {
+        [BindProperty]
+        public User Users { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
         }
     }
 }
